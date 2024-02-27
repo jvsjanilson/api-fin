a1b5f64140ac7fdce0ca44c5eb2b3b51685bdf1b
Janilson Varela <janilsonjvs@gmail.com>
2024-02-27 19:55:55 -0300

    feat: migrate e model city

---
+++ database/migrations/2024_02_27_224014_create_cities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('description', 120);
+            $table->integer('cod_city');
+            $table->string('state_acronym', 3);
+            $table->integer('cod_state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
